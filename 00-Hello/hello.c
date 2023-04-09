70b550215eedac36c2450fbc50386210c73a3d15
lucasciancio <129192526+lucasciancio@users.noreply.github.com>
2023-04-08 23:08:43 -0300

    Add files via upload

---
+++ 00-Hello/hello.c
@@ -0,0 +1,12 @@
+// Programa Hello C
+// Muestra en pantalla el mensaje "Hello, world!"
+// Autor: Lucas Ciancio
+// Última modificacion: 
+
+
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello, world!\n");
+    return 0;
+}
