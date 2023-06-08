24622f4ba40ecdf806ad39bc6ba37722e46305cb
lucasciancio <lucas.ciancio2@gmail.com>
2023-06-08 14:02:30 -0300

    Agrego tests para las funciones de conversión.

--- 01-Temperaturas/ConversionTest.c
+++ 01-Temperaturas/ConversionTest.c
@@ -3,12 +3,22 @@
 #include <assert.h>
 #include "Conversion.h"
 
-const float epsilon = 0.0001; 
+const float epsilon = 0.001; 
 
 
 int main(void) {
     
+    // Pruebas función Celsius
 
+    assert(floatcomp(Celius(0.0),-17.8) == 1);
+    assert(floatcomp(Celius(160.0),71.1) == 1);
+    assert(floatcomp(Celius(300.0),148.9) == 1);
+
+    // Pruebas función Fahrenheit
+
+    assert(floatcomp(Fahrenheit(0.0),32.0) == 1);
+    assert(floatcomp(Fahrenheit(160.0),320.0) == 1);
+    assert(floatcomp(Fahrenheit(300.0),572.0) == 1);
 
     return 0;
 }
@@ -18,3 +28,30 @@ int main(void) {
 int floatcomp(float x, float y) {
     return abs(x-y) < epsilon;
 }
+
+
+
+// TABLAS DE CASOS DE PRUEBA
+
+/*
+
+FUNCIÓN CONVERTIR A CELSIUS
+
+fórmula: °C = 5/9 * (°F - 32)
+
+FAHR   |CEL
+0.0    |-17.8
+160.0  |71.1 
+300.0  |148.9
+
+FUNCIÓN CONVERTIR A FAHRENHEIT
+
+fórmula °F = °C * 9/5 + 32
+
+CEL    |FAHR
+0.0    |32.0
+160.0  |320.0
+300.0  |572.0
+
+
+*/
