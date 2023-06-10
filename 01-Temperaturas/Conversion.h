aa8ba90479334a907754af33620e0050b7b8189a
lucasciancio <lucas.ciancio2@gmail.com>
2023-06-09 21:15:55 -0300

    Conversion.h y tablas

---
+++ 01-Temperaturas/Conversion.h
@@ -0,0 +1,7 @@
+#ifndef CONVERSION_H
+#define CONVERSION_H
+
+float Celsius(float fahr);
+float Fahrenheit(float celsius);
+
+#endif
---
+++ 01-Temperaturas/TablasDeConversion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "Conversion.h"
+
+const int paso = 20;
+const int minimo = 0;
+const int maximo = 300;
+
+void TablaCel_Fahr();
+void TablaFahr_Cel();
+
+
+int main (void) {
+
+    TablaCel_Fahr();
+    
+    TablaFahr_Cel();
+
+    return 0;
+}
+
+
+
+void TablaCel_Fahr() {
+    printf("Tabla de conversión de celsius a fahrenheit\n\n");
+    printf("%-6s \t %-6s \n", "°C", "°F");
+    
+    float fahr;
+    
+    for (float celsius = minimo; celsius <= maximo; celsius += paso) {
+        fahr = Fahrenheit(celsius);
+        printf("%-6.1f \t %-6.1f \n", celsius, fahr);
+    }
+
+    printf("\n\n\n");
+}
+
+
+
+
+void TablaFahr_Cel() {
+    printf("Tabla de conversión de fahrenheit a celsius\n\n");
+    printf("%-6s \t %-6s \n", "°F", "°C");
+
+    float celsius;
+
+    for (float fahr = minimo; fahr <= maximo; fahr += paso) {
+        celsius = Celsius(fahr);
+        printf("%-6.1f \t %-6.1f \n", fahr, celsius);
+    }
+
+    printf("\n\n\n");
+}
